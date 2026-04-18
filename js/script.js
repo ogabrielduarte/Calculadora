021c6a9e3b76d0f1256a37ebe178e1d6d444d072
ogabrielduarte <gsdo1@aluno.ifal.edu.br>
2026-04-18 17:46:42 -0300

    Evento de clique de botões adicionado
    É possível analisar o funcionamento do display, mas as somente como campo de texto.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
+const display = document.querySelector('.display');
+const botoes = document.querySelector('.botoes');
 
+botoes.addEventListener('click', (event) => {
+    const valorBotao = event.target.dataset.valor;
+
+    if (event.target.tagName == 'BUTTON') {
+        display.value += valorBotao;
+    }
+})
