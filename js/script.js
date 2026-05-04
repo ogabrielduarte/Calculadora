eecb7652ed56c370c74c8619c54d68eee6769184
ogabrielduarte <gsdo1@aluno.ifal.edu.br>
2026-05-04 16:51:15 -0300

    Atualização inicial da lógica de operação
    Ainda está muito incompleto. As operações estão completamente bugadas.
    Botão C e AC funcionam, mas suas funções ainda não possuem diferença lógica no programa. Quando configurados, C limpará tão somente o display, e AC vai limpar tudo: display, memória e qualquer valor armazenado.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,46 @@
 const display = document.querySelector('.display');
 const botoes = document.querySelector('.botoes');
 
+let a = 0,
+    b = 0,
+    operadorSelecionado = null;
+
+let estado = false;
+
+const operacoes = {
+    '+': (a, b) => a + b,
+    '-': (a, b) => a - b,
+    '*': (a, b) => a * b,
+    '/': (a, b) => a / b,
+}
+
+function calcular(a, b, operador) {
+    return operacoes[operador](a, b)
+}
+
 botoes.addEventListener('click', (event) => {
     const valorBotao = event.target.dataset.valor;
 
-    if (event.target.tagName == 'BUTTON') {
-        display.value += valorBotao;
+    if (event.target.classList.contains('numero')) {
+        if (estado) {
+            display.value = valorBotao;
+            estado = false;
+        } else {
+            display.value += valorBotao;
+        }
+    } else if (event.target.classList.contains('ac') || event.target.classList.contains('c')) {
+        display.value = ''
+    } else if (event.target.classList.contains('operacao')) {
+        let valorAtual = Number(display.value);
+
+        if (a != 0 && operadorSelecionado != null) {
+            a = calcular(a, valorAtual, operador);
+            display.value = a;
+        } else {
+            a = valorAtual;
+        }
+
+        operadorSelecionado = valorBotao;
+        estado = true;
     }
 })
