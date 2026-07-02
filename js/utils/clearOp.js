98aef094d312abd12a9b995cf4ff0be96c5b61aa
ogabrielduarte <gabriel.sduarte.oli@gmail.com>
2026-07-02 14:42:25 -0300

    feat: add All Clear (AC button) function

---
+++ js/utils/clearOp.js
@@ -0,0 +1,10 @@
+export function allClear(c, d) {
+    c.slotA = null;
+    c.slotB = null;
+    c.memoryA = null;
+    c.memoryB = null;
+    c.currentOperation = null;
+    c.status = false;
+
+    d.value = ''
+}
